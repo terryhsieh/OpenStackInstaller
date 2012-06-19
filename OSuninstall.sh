67a38b0d5468341cd258b31592e9296072e74c28
root <root@management1.(none)>
2012-06-19 17:15:27 +0800

    re-orgnize folder tree

--- OSuninstall.sh
+++ OSuninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Really simple (and gung-ho) script to remove OpenStack
-apt-get remove nova-compute nova-network nova-api nova-objectstore keystone glance nova-scheduler
-mysql -uroot -popenstack -e "drop database if exist nova;"
-mysql -uroot -popenstack -e "drop database if exist glance;"
-mysql -uroot -popenstack -e "drop database if exist keystone;"
-apt-get autoremove
+#apt-get remove nova-compute nova-network nova-api nova-objectstore keystone glance nova-scheduler
+mysql -uroot -popenstack -e "drop database if exists nova;"
+mysql -uroot -popenstack -e "drop database if exists glance;"
+mysql -uroot -popenstack -e "drop database if exists keystone;"
+#apt-get autoremove
